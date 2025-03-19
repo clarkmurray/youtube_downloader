8e93d5d213421acc8519f05bee31e1aa5dd55677
Clark Murray <clarkmurray@Clarks-2019-MacBook-Pro.local>
2025-03-18 20:05:31 -0400

    Convert to using pytube-fix instead of pytube (pytube is no longer maintained

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,13 +1,31 @@
-from pytube import YouTube
+# from pytube import YouTube
+# from sys import argv
+
+# link = argv[1] # First command line argument; argv[0] is always the program name
+
+# yt = YouTube(link)
+
+# stream = yt.streams.get_highest_resolution()
+
+# print("Downloading \"" + yt.title + "\" from " + yt.author + " in " + stream.resolution)
+# stream.download("/Users/clarkmurray/Movies/YouTube Downloads")
+# print("Download is complete")
+
+
+
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 from sys import argv
 
-link = argv[1] # First command line argument; argv[0] is always the program name
 
-yt = YouTube(link)
+# url = "url"
 
-stream = yt.streams.get_highest_resolution()
+url = argv[1] # First command line argument; argv[0] is always the program name
 
-print("Downloading \"" + yt.title + "\" from " + yt.author + " in " + stream.resolution)
-stream.download("/Users/clarkmurray/Movies/YouTube Downloads")
+yt = YouTube(url, on_progress_callback=on_progress)
+
+print(yt.title)
+# print("Downloading \"" + yt.title + "\" from " + yt.author + " in " + stream.resolution)
+ys = yt.streams.get_highest_resolution()
+ys.download()
 print("Download is complete")
-
